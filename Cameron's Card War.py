33dd0c980377202fa1487e83b8e62731fac75196
CameronFr02 <cam.fraser@rocketmail.com>
2017-01-26 11:46:28 +0000

    Add files via upload

---
+++ Cameron's Card War.py
@@ -0,0 +1,31 @@
+
+print("What deck size would you like, 3, 6 or 10?")
+
+deck_size = 0
+
+player_deck_size = input()
+
+if player_deck_size == ("3"):
+    deck_size = 3
+if player_deck_size == ("6"):
+    deck_size = 6
+if player_deck_size == ("10"):
+    deck_size = 10
+if player_deck_size == ("1"):
+    print("invalid deck size")
+if player_deck_size == ("2"):
+    print("invalid deck size")
+if player_deck_size == ("4"):
+    print("invalid deck size")
+if player_deck_size == ("5"):
+    print("invalid deck size")
+if player_deck_size == ("7"):
+    print("invalid deck size")
+if player_deck_size == ("8"):
+    print("invalid deck size")
+if player_deck_size == ("9"):
+    print("invalid deck size")
+print(deck_size)
+
+        
+        
